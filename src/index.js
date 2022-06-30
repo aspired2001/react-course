496747c5882d64a95da4b4c9650e5e2fb2f3dc62
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2022-06-30 14:31:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import react from "react";
+import reactDOM from "react-dom";
+import "./download.jpg";
+import "./download (1).jpg";
+import "./download (2).jpg";
+reactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Food</h1>
+    <div>
+      <img src="" />
+    </div>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Nooodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
